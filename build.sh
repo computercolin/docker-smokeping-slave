cb409bfaf7782f7b70c82b94471576adaae5d6c6
computercolin <computercolin@gmail.com>
2022-04-14 02:38:33 -0400

    Only increment build version when explicitly requested.

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-OLDVERSION=$(cat VERSION)
-VERSION=$((${OLDVERSION} + 1))
-BUILD_DATE=$(date)
+VERSION=$(cat VERSION)
+if [ "$1" = "--new" ]; then
+  VERSION=$((${VERSION} + 1))
+fi
 echo ${VERSION} > VERSION
+
 docker build --build-arg VERSION="$VERSION" --build-arg BUILD_DATE="$BUILD_DATE" --no-cache --pull -t computercolin/smokeping-slave:latest -t computercolin/smokeping-slave:${VERSION} .
