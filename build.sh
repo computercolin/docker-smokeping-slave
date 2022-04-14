00243fc5b1496f86b098a95b27d4c304aa2819a7
computercolin <computercolin@gmail.com>
2022-04-14 01:47:16 -0400

    Remove cullorblind docker tags

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ OLDVERSION=$(cat VERSION)
 VERSION=$((${OLDVERSION} + 1))
 BUILD_DATE=$(date)
 echo ${VERSION} > VERSION
-docker build --build-arg VERSION="$VERSION" --build-arg BUILD_DATE="$BUILD_DATE" --no-cache --pull -t cullorblind/smokeping-slave:latest -t cullorblind/smokeping-slave:${VERSION} .
+docker build --build-arg VERSION="$VERSION" --build-arg BUILD_DATE="$BUILD_DATE" --no-cache --pull -t computercolin/smokeping-slave:latest -t computercolin/smokeping-slave:${VERSION} .
